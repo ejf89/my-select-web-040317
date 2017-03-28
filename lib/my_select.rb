88c7681e5918a94113a59f9e383885f18c92f083
Eric Farber <eric.j.farber@gmail.com>
2017-03-28 01:56:41 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,21 @@
-def my_select(collection)
- # your code here!
+require 'pry'
+
+
+def my_select(array)
+    i = 0
+    selection = []
+    while i < array.length
+        if yield(array[i]) == true
+            selection << array[i]
+        end
+        i = i + 1
+    end
+    selection
+end
+
+
+array = [1, 24, 3]
+
+my_select(array) do |element|
+    element.even?
 end
